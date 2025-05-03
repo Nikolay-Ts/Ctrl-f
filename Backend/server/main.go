050560adc8e180dcb006a8338911a0381bae5137
Rumen <rumenmitov@protonmail.com>
2025-05-03 17:48:14 +0200

    uploading files

--- Backend/server/main.go
+++ Backend/server/main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Server is not up yet :P"))
+	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseMultipartForm(20 << 20) // 20 MB limit
+
+		files := r.MultipartForm.File["pdfs"]
+		if len(files) == 0 {
+			http.Error(w, "No files uploaded", http.StatusBadRequest)
+			return
+		}
+
+		for _, fileHeader := range files {
+			file, err := fileHeader.Open()
+			if err != nil {
+				http.Error(w, "Error opening file", http.StatusInternalServerError)
+				return
+			}
+			defer file.Close()
+
+			dst, err := os.Create("./uploads/" + fileHeader.Filename)
+			if err != nil {
+				http.Error(w, "Error creating file", http.StatusInternalServerError)
+				return
+			}
+			defer dst.Close()
+
+			_, err = io.Copy(dst, file)
+			if err != nil {
+				http.Error(w, "Error saving file", http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Fprintf(w, "Uploaded: %s\n", fileHeader.Filename)
+		}
 	})
 
 	port := os.Getenv("PORT")
