5ff6bc379910d4e9a40fb816605ab9e5c904f276
Rumen <rumenmitov@protonmail.com>
2025-05-04 01:36:47 +0200

    backend: create data dir

--- backend/server/types.go
+++ backend/server/types.go
@@ -35,10 +35,15 @@ type UniqueDir struct {
 
 // UniqueDir.New creates a uniquely named directory.
 func (dir *UniqueDir) New() error {
+	err := os.Mkdir("data", 0770)
+	if err != nil {
+		return err
+	}
+
 	uuid := fmt.Sprintf("%d-%d", time.Now().UnixNano(), rand.Int())
 	dir.Path = path.Join("data", uuid)
 
-	err := os.Mkdir(dir.Path, 0770)
+	err = os.Mkdir(dir.Path, 0770)
 	if err != nil {
 		return err
 	}
