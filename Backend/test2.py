4850a97c4379ae374091b528149613b4700d953d
FelipeRibadeneira <fribadenei@constructor.university>
2025-05-03 11:26:30 +0200

    Test with Gemini

--- Backend/test2.py
+++ Backend/test2.py
@@ -1,28 +1,20 @@
+from llama_index.core.agent.workflow import FunctionAgent
+from llama_index.llms.ollama         import Ollama
 import asyncio
-from llama_index.agent import FunctionAgent
-from llama_index.llms import Ollama
 
-
-# Define a simple calculator tool
+# Your multiplication tool
 def multiply(a: float, b: float) -> float:
-    """Useful for multiplying two numbers."""
     return a * b
 
-
-# Create an agent workflow with our calculator tool
 agent = FunctionAgent(
     tools=[multiply],
-    llm=Ollama(model="llama3.1", request_timeout=360.0),
+    llm=Ollama(model="llava:latest", request_timeout=360.0),
     system_prompt="You are a helpful assistant that can multiply two numbers.",
 )
 
-
 async def main():
-    # Run the agent
     response = await agent.run("What is 1234 * 4567?")
-    print(str(response))
+    print(response)
 
-
-# Run the agent
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ Backend/testgemini.py
@@ -0,0 +1,35 @@
+from google import genai
+from google.genai import types
+import os
+
+os.environ["API_KEY"] = 'AIzaSyDGyO1GFfxydCDvx0AFPbmOlR6-fABQV44'
+
+client = genai.Client(api_key=os.environ["API_KEY"])
+#response = client.models.generate_content(
+#    model='gemini-2.0-flash',
+#    contents='what does pandas package do?'
+#)
+#print(response.text)
+
+
+with open(r'C:\Users\ribad\OneDrive - Constructor University\GDGHack\Ctrl-f\data\practice_sheet.pdf', 'rb') as pdf_file:
+    pdf_data = pdf_file.read()
+
+# Suppose you get `value` from user or code:
+value = input("Enter the term to highlight: ")  
+
+# Build the prompt with an f-string:
+prompt = f'Identify and highlight all instances of "{value}" in the document and give me the page number in the pdf.'
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[
+        types.Part.from_bytes(
+            data=pdf_data,
+            mime_type='application/pdf',
+        ),
+        prompt
+    ]
+)
+
+print(response.text)
